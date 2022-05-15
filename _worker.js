b9556d94ace6d9a2228c9bb922900c85bc4a7ff0
kylehao <kylehao@gmail.com>
2022-05-15 23:26:42 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="book.g.free.fr";
+        url.hostname="fast-bee.pikapod.net";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
